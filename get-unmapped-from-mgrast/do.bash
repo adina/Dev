2a32e558bf98816b6387dbf5c359f60a2b068b82
root <root@the-most-awsome-machine.novalocal>
2012-11-05 19:49:59 +0000

    small changes

--- get-unmapped-from-mgrast/do.bash
+++ get-unmapped-from-mgrast/do.bash
@@ -1,4 +1,4 @@
 #gzip -d 650*gz
 #gzip -d 350*gz
-python get-list-of-hits.py 650*expand.protein > list-of-orfs-hits.txt
+python get-list-of-hits.py 650*sims > list-of-orfs-hits.txt
 java -jar /adina/Dev/remove-reads/Clustering.jar filter-seqs list-of-orfs-hits.txt 550*.faa True > noannotation-orfs.faa
--- get-unmapped-from-mgrast/get-list-of-hits.py
+++ get-unmapped-from-mgrast/get-list-of-hits.py
@@ -3,10 +3,7 @@ import sys
 d = {}
 for line in open(sys.argv[1]):
     line = line.rstrip().split('\t')
-    orf = line[1]
-    evalue = float(line[4])
-    if evalue < 1e-3:
-        d[orf] = 1
-
+    orf = line[0]
+    d[orf] = ''
 for key in d.keys():
     print key

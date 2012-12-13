5e16e9c5150742da7d00351890a7045c97ac41e4
adina <howead@msu.edu>
2012-12-13 13:37:28 -0500

    coverage dist hist

--- plotting/get-coverage-dist.py
+++ plotting/get-coverage-dist.py
@@ -6,28 +6,33 @@ import matplotlib.pyplot as plt
 
 
 list = []
-for record in screed.open(sys.argv[1]):
-    cov = float(record.name.split('cov=')[1][:-1])
-    length = len(record.sequence)
-    data = (cov, length)
-    list.append(data)
+list2 = []
+for line in open(sys.argv[1]):
+    line = float(line.rstrip().split(' ')[1])
+    list.append(line)
+for line in open(sys.argv[2]):
+    line = float(line.rstrip().split(' ')[1])
+    list2.append(line)
 
-#histogram of coverage distribution
-hist_list = []
-
-for x in list:
-    hist_list.append(x[0])
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-n, bins, patches = ax.hist(hist_list, 300, cumulative=False, normed=False, alpha=0.8)
 
-plt.xlabel('Contig Coverage - Median bp')
-plt.ylabel('# of Contigs with Specified Coverage')
-plt.axis([0,100,0,300000])
-plt.title(sys.argv[1] + ' Assembly Coverage Distribution')
-#plt.show()
-plt.savefig(sys.argv[1] + '.coverage.pdf')
+ax = fig.add_subplot(211)
+ax.hist(list, 2200, cumulative=False, normed=True, alpha=0.8, color='g')
+ax.set_xlim(0,50)
+ax.set_ylim(0,0.10)
+ax.set_ylabel('Relative abundance')
+
+ax2 = fig.add_subplot(212)
+ax2.hist(list2, 1450, cumulative=False, normed=True, alpha=0.8, color='m')
+ax2.set_xlim(0,50)
+ax2.set_ylim(0,0.10)
+ax2.set_xlabel('Median basepair coverage of assembled contigs')
+ax2.set_ylabel('Relative abundance')
+#plt.axis([0,50,0,0.10])
+#plt.title(sys.argv[1] + ' Assembly Coverage Distribution')
+
+plt.savefig('fig3-coverage.eps', dpi=300)
 
 
 

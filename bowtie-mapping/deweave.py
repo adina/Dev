a69fa538f0f77da5022d2175e647976b3145130a
adina <howead@msu.edu>
2012-12-14 10:31:42 -0500

    fixed bug

--- bowtie-mapping/deweave.py
+++ bowtie-mapping/deweave.py
@@ -8,5 +8,6 @@ paired_file2 = open(sys.argv[1] + '.pe2', 'w')
 for record in screed.open(paired_file):
     if record.name.endswith('/1'):
         paired_file1.write('@%s\n%s\n+\n%s\n' % (record.name, record.sequence, record.accuracy))
+    else:
         paired_file2.write('@%s\n%s\n+\n%s\n' % (record.name, record.sequence, record.accuracy))
  

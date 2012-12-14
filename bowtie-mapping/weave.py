73909656d52f59970e2934642e2f4bafdb94f2e0
adina <howead@msu.edu>
2012-12-14 10:27:44 -0500

    bowtie2 prep scripts

---
+++ bowtie-mapping/weave.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+import screed, sys, itertools
+
+paired_file = sys.argv[1]
+paired_file1 = open(sys.argv[1] + '.pe1', 'w')
+paired_file2 = open(sys.argv[1] + '.pe2', 'w')
+
+for record in screed.open(paired_file):
+    if record.name.endswith('/1'):
+        paired_file1.write('@%s\n%s\n+\n%s\n' % (record.name, record.sequence, record.accuracy))
+        paired_file2.write('@%s\n%s\n+\n%s\n' % (record.name, record.sequence, record.accuracy))
+ 

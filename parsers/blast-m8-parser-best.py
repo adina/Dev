fa140171b7d8d9de361e1426fea77de4418dd134
adina <howead@msu.edu>
2013-04-15 10:25:23 -0400

    fixed best parser

--- parsers/blast-m8-parser-best.py
+++ parsers/blast-m8-parser-best.py
@@ -6,7 +6,7 @@ for line in open(sys.argv[1]):
     data = line.rstrip().split('\t')
     query = data[0]
     hit = data[1]
-    if d.has_query(hit):
+    if d.has_key(query):
         continue
     else:
         d[query] = hit

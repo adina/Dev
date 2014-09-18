6f6ed7a94305e58cdbc5f86f5fe264b41cef81fa
root <root@howe-base-iii.novalocal>
2014-09-18 15:06:39 +0000

    some scripts

--- anl-servers/do.sh
+++ anl-servers/do.sh
@@ -1,3 +1,3 @@
-apt-get install emacs
 apt-get -y update
 apt-get -y install git emacs mercurial less python-matplotlib unzip bzip2 zlib1g-dev ncurses-dev python-dev build-essential libxml-parser-perl
+apt-get -y install python-numpy python-scipy 

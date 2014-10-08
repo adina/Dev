a1ff93b1d4eaab9a57ff0c31bc6945ab381dc5c3
adina <adina.chuang@gmail.com>
2014-10-08 11:23:02 -0500

    Update install-khmer.sh

--- anl-servers/install-khmer.sh
+++ anl-servers/install-khmer.sh
@@ -1,18 +1,15 @@
 apt-get -y update
 apt-get -y install git emacs mercurial less python-matplotlib unzip bzip2 zlib1g-dev ncurses-dev python-dev build-essential libxml-parser-perl
 
-curl -O http://python-distribute.org/distribute_setup.py
-python distribute_setup.py 
-easy_install nose
+git clone https://github.com/ged-lab/khmer.git
+cd khmer
+git checkout v1.1
+make install
 
-git clone git://github.com/ctb/screed.git
+cd 
+git clone git://github.com/ged-lab/screed.git
 cd screed
-python setup.py install 
-
-cd ..
-git clone git://github.com/ctb/khmer.git
-cd khmer 
-make clean all
+python setup.py install
 
 cd
 echo export PYTHONPATH=/mnt/khmer/python >> /root/.bashrc

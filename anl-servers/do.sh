1271d2c04f1a10c5f3337e2365643da860cc7565
adina <adina.chuang@gmail.com>
2014-10-09 10:33:00 -0500

    Update do.sh

--- anl-servers/do.sh
+++ anl-servers/do.sh
@@ -1,7 +1,7 @@
 apt-get -y update
 apt-get -y install git emacs mercurial less python-matplotlib unzip bzip2 zlib1g-dev ncurses-dev python-dev build-essential libxml-parser-perl
 apt-get -y install python-numpy python-scipy 
-
+apt-get -y install libhdf5-serial-dev
 #Installing R
 sudo apt-get upgrade
 apt-get install r-base

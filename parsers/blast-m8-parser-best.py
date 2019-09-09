f964d9fbfe605edf8a7ee7515f84a2ed78ea5b6d
Adina Howe <adina@users.noreply.github.com>
2019-09-09 08:45:41 -0500

    Update blast-m8-parser-best.py

--- parsers/blast-m8-parser-best.py
+++ parsers/blast-m8-parser-best.py
@@ -6,8 +6,11 @@ for line in open(sys.argv[1]):
     data = line.rstrip().split('\t')
     query = data[0]
     hit = data[1]
+    evalue = float(data[-2])
     if d.has_key(query):
         continue
     else:
         d[query] = hit
-        print line, 
+        if evalue <= 1e-5:
+            print line, 
+        
